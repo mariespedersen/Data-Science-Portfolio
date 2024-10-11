23071b412c8bb720ccf36542f77a53a79a29f4a9
Marie Sjørslev Pedersen <marie.sp.1996@gmail.com>
2024-10-11 12:14:18 +0200

    add basic packages

--- Packages.py
+++ Packages.py
@@ -1,5 +1,21 @@
 import kaggle
 
+# Data wrangling
+import numpy as np
+import pandas as pd
+
+# Data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Machine learning
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_auc_score, roc_curve, auc, f1_score, precision_score, recall_score, precision_recall_curve, average_precision_score
+
+# Models
+from sklearn.linear_model import LogisticRegression
+
 ####################################
 # Print
 ####################################
