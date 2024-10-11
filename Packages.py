b9734062e145bdd60e95b6a9ec265b9ca24aff97
Marie Sjørslev Pedersen <marie.sp.1996@gmail.com>
2024-10-11 11:02:23 +0200

    Kaggle API Setup

---
+++ Packages.py
@@ -0,0 +1,13 @@
+####################################
+# Setup Kaggle API
+####################################
+
+# Step 1: Open and load the JSON file
+import json
+with open('kaggle.json', 'r') as file:
+    kaggle_info = json.load(file)
+    
+# Step 2: Set the Kaggle API key
+import os
+os.environ['KAGGLE_USERNAME'] = kaggle_info['username']
+os.environ['KAGGLE_KEY'] = kaggle_info['key']
