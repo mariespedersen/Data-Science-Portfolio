e7332aa7284e077111baa9aa75714ed17c45cf3b
Marie Sjørslev Pedersen <marie.sp.1996@gmail.com>
2024-10-11 11:42:25 +0200

    removed API key

--- Packages.py
+++ Packages.py
@@ -1,13 +1,6 @@
-####################################
-# Setup Kaggle API
-####################################
+import kaggle
 
-# Step 1: Open and load the JSON file
-import json
-with open('../kaggle.json', 'r') as file:
-    kaggle_info = json.load(file)
-    
-# Step 2: Set the Kaggle API key
-import os
-os.environ['KAGGLE_USERNAME'] = kaggle_info['username']
-os.environ['KAGGLE_KEY'] = kaggle_info['key']
+####################################
+# Print
+####################################
+print("Packages.py has been loaded.")
